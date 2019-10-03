3c2a645c85f4e902098b8a90fb0bd54deb40d831
maximusnikulin <perseusxii@gmail.com>
2019-10-03 21:16:31 +0700

    Remove dist folder

--- classes/Positioner/__test__/Positioner.spec.ts
+++ classes/Positioner/__test__/Positioner.spec.ts
@@ -1,5 +1,6 @@
 describe('Test Positioner', () => {
-  test('It should', () => {})
-  test('It should ', () => {})
-  test('It should ', () => {})
+  test('It should check if we can change vertical position of Figure', () => {})
+  test('It should check if we can change horizontal position of Figure', () => {})
+  test('It should check if we can shrink rows of pointstack', () => {})
+  test('It should check if we have equalRows of pointsstack', () => {})
 })
